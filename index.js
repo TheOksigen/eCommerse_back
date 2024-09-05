308c1c3a15d159316a2d574d335bca66ae29f9de
@david <david@yetim.me>
2024-09-05 21:51:25 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const productsRouter = require('./src/routes/products.route');
+const loginRouter = require('./src/routes/login.route');
+const categoriesRouter = require('./src/routes/category.route');
+const cors = require('cors');
+const { setupSwagger } = require('./src/swagger');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/', loginRouter);
+app.use('/products', productsRouter);
+app.use('/categories', categoriesRouter);
+
+setupSwagger(app);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
